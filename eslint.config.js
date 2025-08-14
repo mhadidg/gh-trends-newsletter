2056d06b2ad680e2986ee8e9e10d5574cc92ec13
Mustapha Hadid <mustapha.hadeed@gmail.com>
2025-08-15 00:22:30 +0300

    Ignore "@eslint/js" is not listed in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+// noinspection NpmUsedModulesInstalled
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
@@ -27,20 +28,20 @@ export default [
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
-      
+
       // TypeScript specific rules
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-floating-promises': 'error',
-      
+
       // Import rules
       'import/order': 'error',
       'import/no-duplicates': 'error',
-      
+
       // Promise rules
       'promise/catch-or-return': 'error',
       'promise/param-names': 'error',
-      
+
       // General rules
       'no-console': 'warn',
       'prefer-const': 'error',
