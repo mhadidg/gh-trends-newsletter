5898f662f048c6306fd7b6aa810527c8212be71f
Mustapha Hadid <mustapha.hadeed@gmail.com>
2025-08-17 22:47:57 +0300

    Supress info logs in test runner

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -2,5 +2,6 @@ import { vi, beforeEach } from 'vitest';
 
 // Run before each test as vi.resetAllMocks() clears it
 beforeEach(() => {
-  vi.spyOn(console, 'error').mockImplementation(() => {}); // Suppress console.error in tests
+  vi.spyOn(console, 'log').mockImplementation(() => {}); // Suppress info logs
+  vi.spyOn(console, 'error').mockImplementation(() => {}); // Suppress error logs
 });
