51a426e686f50fb6ed7347cbf5d85a33c0c2a8cd
Mustapha Hadid <mustapha.hadeed@gmail.com>
2025-08-18 19:21:33 +0300

    Configure # of repos to feature in the newsletter

--- src/pipeline/score.ts
+++ src/pipeline/score.ts
@@ -1,9 +1,11 @@
 import { Repository, ScoredRepository } from '../utils/types';
 
 export function score(repos: Repository[]): ScoredRepository[] {
+  const topn = parseInt(process.env.NEWSLETTER_TOP_N || '10');
+
   // Simple logic for MVP
   return repos
-    .slice(0, 10)
+    .slice(0, topn)
     .map(repo => ({
       ...repo,
       score: repo.stargazerCount,
