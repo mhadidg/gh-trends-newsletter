1fa18c222a3f2a9b24ee68d9b4082dce41eb0b48
Mustapha Hadid <mustapha.hadeed@gmail.com>
2025-08-22 23:07:52 +0300

    Supress warn logs in test runner

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -3,7 +3,8 @@ import { vi, beforeEach } from 'vitest';
 // Must before each test; otherwise, vi.resetAllMocks() would clear it
 beforeEach(() => {
   // Suppress logs in tests
-  vi.spyOn(console, 'info').mockImplementation(() => {});
   vi.spyOn(console, 'log').mockImplementation(() => {});
+  vi.spyOn(console, 'info').mockImplementation(() => {});
+  vi.spyOn(console, 'warn').mockImplementation(() => {});
   vi.spyOn(console, 'error').mockImplementation(() => {});
 });
